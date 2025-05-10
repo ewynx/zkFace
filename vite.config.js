08e39baa5d0b056525db81e60dff633ee58f2d4a
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2025-05-09 20:02:08 -0600

    Add deployment config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/zkFace/",
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: { target: "esnext" },
